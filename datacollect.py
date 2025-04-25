429891585c9f06186e55db458b22b1c64cacf1dc
Sumit Kumar Singh <115483120+RAJPUTRoCkStAr@users.noreply.github.com>
2025-04-25 14:00:08 +0530

    Update datacollect.py

--- datacollect.py
+++ datacollect.py
@@ -1,15 +1,18 @@
 import streamlit as st
 import pandas as pd
 import re
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from datetime import datetime
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 def init_driver():
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
+    options = Options()
+    options.add_argument("--headless")  
+    options.add_argument("--no-sandbox") 
+    options.add_argument("--disable-dev-shm-usage") 
+    options.add_argument("--disable-gpu")  
+
     return webdriver.Chrome(options=options)
 
 def get_text_safe(driver, xpath, multiple=False):
